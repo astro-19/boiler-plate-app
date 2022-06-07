1d1b91f45c360316ab20694524fd2bfa0d52d22b
Pushkar Pandey <pushkartas2418@gmail.com>
2022-06-07 12:36:59 +0530

    Create-react-app boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,6 @@ import App from './App';
 
 test('renders react', () => {
   render(<App />);
-  // const linkElement = screen.getByRole('div')
-  // expect(linkElement).toHaveTextContent('It\'s working...')
-
   const element = screen.getByRole('div')
   expect(element).toHaveTextContent('It\'s working...')
 });
